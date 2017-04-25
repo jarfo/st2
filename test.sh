c055fe8a143b30c0ea74722a62e356bbfcdb16b8
adrian <jose.fonollosa@upc.edu>
2017-04-25 17:35:57 +0200

    test.sh

--- test.sh
+++ test.sh
@@ -3,23 +3,34 @@
 ./tokenizer.perl -l en < newstest2016-deen-ref.en > newstest2016-deen-ref.en.tok
 ./tokenizer.perl -l en < newstest2015-deen-ref.en > newstest2015-deen-ref.en.tok
 
-corpora="news-commentary-v12.en news.2015.en.shuffled"
+corpora="
+  http://data.statmt.org/wmt17/translation-task/news-commentary-v12.en.gz
+  http://data.statmt.org/wmt16/translation-task/news.2015.en.shuffled.gz
+"
 
-for corpus in $corpora; do
+for url in $corpora; do
+  corpus=`basename $url`
+  corpus=${corpus%.*}
   if [ ! -f $corpus.tok ]; then
-    wget http://data.statmt.org/wmt17/translation-task/$corpus.gz
+    wget $url 
     gunzip -c $corpus.gz | ./tokenizer.perl -l en > $corpus.tok
   fi
 done
 
-for corpus in $corpora; do
+for url in $corpora; do
+  corpus=`basename $url`
+  corpus=${corpus%.*}
   ngram-count -order 4 -text $corpus.tok -lm $corpus.4.arpa
   ngram -order 4 -lm $corpus.4.arpa -ppl newstest2016-deen-ref.en.tok
   ngram -order 4 -lm $corpus.4.arpa -ppl newstest2015-deen-ref.en.tok -debug 2 > $corpus.4.ppl
 done
 
 devppl=""
-for corpus in $corpora; do
+for url in $corpora; do
+  corpus=`basename $url`
+  corpus=${corpus%.*}
   devppl="$devppl $corpus.4.ppl"
 done
 compute-best-mix $devppl
+
+ngram -order 4 -lm news.2015.en.shuffled.4.arpa -mix-lm news-commentary-v12.en.4.arpa -lambda 0.92 -ppl newstest2016-deen-ref.en.tok
